b588d9df0b8661197e124969caf7fab2f3a2a549
Prajakta Deokule <70521908+Prajakta456@users.noreply.github.com>
2022-07-12 15:26:21 +0530

    SIGNUP AND LOGIN NOT REQUIRED

--- MainActivityRegister.java
+++ MainActivityRegister.java
@@ -1,14 +1,12 @@
 package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 public class MainActivityRegister extends AppCompatActivity {
 
     EditText username,password,retypepassword;
@@ -64,10 +62,6 @@ public class MainActivityRegister extends AppCompatActivity {
                         Toast.makeText(MainActivityRegister.this, "Passwords not matching!", Toast.LENGTH_SHORT).show();
                     }
                 }
-
-
-
-
             }
         });
 
