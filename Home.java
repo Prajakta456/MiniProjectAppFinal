ff2d0a18ab3ff1ed86c6a216a4c82dbf39b20021
Prajakta Deokule <70521908+Prajakta456@users.noreply.github.com>
2022-08-22 00:36:09 +0530

    Update Home.java

--- Home.java
+++ Home.java
@@ -21,7 +21,7 @@ public class Home extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(Home.this, MainActivityRegister.class);
+                Intent i=new Intent(Home.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
